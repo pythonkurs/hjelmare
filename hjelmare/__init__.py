a5c85c741b7531e06813c6a87ef1dc5193a56a73
MartinHjelmare <marhje52@kth.se>
2013-02-05 15:27:20 +0100

    Updated session 2

--- hjelmare/__init__.py
+++ hjelmare/__init__.py
@@ -1,14 +1,1 @@
-# Gets the NYC escalator outage data from a webservice. Then calculates the fraction that are out for repairs.
-import untangle, requests
-def fraction_repairs():
-    r = requests.get("http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml")
-    xml_file = r.content
-    doc = untangle.parse(xml_file)
-    outages = doc.NYCOutages.outage
-    numberOutages=len(outages)
-    numberRepairs=0
-    for outage in outages:
-        if outage.reason.cdata == "REPAIR":
-            numberRepairs += 1
-    fractionRepairs = float(numberRepairs) / numberOutages
-    return fractionRepairs
+#
--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -1,3 +1,3 @@
-from hjelmare import fraction_repairs
+from hjelmare.session2 import fraction_repairs
 data = fraction_repairs()
 print("Fraction of Repairs in the NYC subway system:"+str(data))
