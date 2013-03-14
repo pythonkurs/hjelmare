afeff303082816197093c983a47469f182ba49ef
MartinHjelmare <marhje52@kth.se>
2013-03-15 00:28:01 +0100

    Problem solved in the serial way

--- scripts/num_factors.py
+++ scripts/num_factors.py
@@ -1,4 +1,7 @@
 import sys
+
+from collections import defaultdict
+
 #The script should print a dictionary
 #This script should take an argument which is a character.
 if len(sys.argv) != 2:
@@ -27,11 +30,25 @@ def factorize(n):
         else:
             p += 1
 
+histogram = defaultdict(int)
+
 integers = range(2, 500001)
+# Testing
+#integers = range(2, 11)
 
 if control_char == "s":
     for integer in integers:
-        print(factorize(integer))
+        unique_count = 1
+        factors = (factorize(integer))
+        #print(factors)
+        old_factor = factors[0]
+        for factor in factors:
+            if not factor == old_factor:
+                unique_count += 1
+            old_factor = factor
+        #print(unique_count)
+        histogram[unique_count] += 1
+    print(histogram)
 else:
     print("You should give one argument which should be a character, s, m or i, which will determine how the script will run. Serially (s), with multiprocessing (m) or with IPython parallel (i).")
     sys.exit()
